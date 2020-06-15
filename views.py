594a837f88b717681c904d1f1ad384bfc097b816
Random Coder <vinibritocoder@gmail.com>
2020-06-15 00:38:25 -0300

    Bônus: adicionando música de fundo

--- views.py
+++ views.py
@@ -6,30 +6,35 @@ import arcade, random, time
 from constants import *
 
 class InitialView(arcade.View):
-    ''' Class to present the game's initial view. '''
+
     def on_show(self):
         arcade.set_background_color(arcade.color.GREEN)
 
 
     def on_draw(self):
-        ''' Print useful informations on the screen.'''
-        # Clear the screen to the background color. It is required to be called
-        # before drawing anything to the screen.
         arcade.start_render()
         self.window.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
-        arcade.draw_text("FLIER FLUBBER", SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50,
+        arcade.draw_text("Flier Flubber", SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
 
+        # Show tip to return or reset
         arcade.draw_text("Pressione ENTER para iniciar",
                          SCREEN_WIDTH/2,
                          SCREEN_HEIGHT/2-30,
                          arcade.color.BLACK,
                          font_size=20,
                          anchor_x="center")
-
+        
+        arcade.draw_text("Pressione F para sair da tela cheia",
+                         SCREEN_WIDTH/2,
+                         SCREEN_HEIGHT/2-50,
+                         arcade.color.BLACK,
+                         font_size=20,
+                         anchor_x="center")
+        
         arcade.draw_text("Pressione Q para sair",
                          SCREEN_WIDTH/2,
-                         SCREEN_HEIGHT/2-60,
+                         SCREEN_HEIGHT/2-70,
                          arcade.color.BLACK,
                          font_size=20,
                          anchor_x="center")
@@ -47,10 +52,15 @@ class InitialView(arcade.View):
 
 
 class MyGame(arcade.View):
-    """ Class that initiates the game. """
+    """
+    Main application class.
+    """
+
     def __init__(self):
+
         # Call the parent class (arcade.View) and set up the window
         super().__init__()
+
         #General attributes
 
         # These are 'lists' that keep track of our sprites. Each sprite should
@@ -58,12 +68,13 @@ class MyGame(arcade.View):
         self.coin_list   = None
         self.wall_list   = None
         self.player_list = None
-
-        # Separate variable that holds the list of player's sprites. Each clone of
-        # the player must be inside this list.
+        self.music         = arcade.load_sound("sounds/racetheme.mp3")
+        self.background  = None
+        
+        # Separate variable that holds the player sprite list
         self.player_sprite  = []
 
-        # Our list of physics engine. Each clone must have its engine.
+        # Our physics engine
         self.physics_engine = []
 
         # Used to keep track of our scrolling
@@ -71,8 +82,9 @@ class MyGame(arcade.View):
         self.view_left   = 0
 
     def on_show(self):
-        arcade.set_background_color(arcade.csscolor.CYAN)
-
+        self.background = arcade.load_texture("images/items/clouds.png")
+        arcade.play_sound(self.music)
+        
     def setup(self):
         """
         The method 'setup' outside __init__ makes it easier to restart the game.
@@ -82,16 +94,15 @@ class MyGame(arcade.View):
         self.window.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
 
         #Create and initiate the attributes:
-        self.obstacle_distance       = 600 #Distnce between obstacles.
-        self.last_position           = 0   #Used to calculate the next obstacle's position.
+        self.obstacle_distance       = 500
+        self.last_position           = 0
         self.level                   = 0
-        self.score                   = 0   #100 pts to level up.
+        self.score                   = 0 #100 pts to level up.
         self.total_score             = 0
-        self.speed                   = 0   #Speed in m/s
-        self.distance                = 0   #computed from speed multiplied by delta_t
+        self.speed                   = 0 #Speed that is measured
+        self.distance                = 0
         self.total_distance          = 0
-        self.initial_time            = time.time() #Used to compute total_distance
-
+        self.initial_time            = time.time()
         # Used to keep track of our scrolling
         self.view_bottom = 0
         self.view_left   = 0
@@ -112,17 +123,17 @@ class MyGame(arcade.View):
         self.wall_list   = arcade.SpriteList(use_spatial_hash=True)
         self.coin_list   = arcade.SpriteList(use_spatial_hash=True)
 
-        # Set up the first sprite of the player, specifically placing it at these coordinates.
+        # Set up the player, specifically placing it at these coordinates.
         self.player_sprite.append(arcade.Sprite("images/player_1/slimeBlue.png", CHARACTER_SCALING))
         self.player_sprite[0].center_x = 64
         self.player_sprite[0].last_x   = 64
         self.player_sprite[0].center_y = SCREEN_HEIGHT
-        self.player_sprite[0].change_x = PLAYER_MOVEMENT_SPEED #Initial movement speed
+        self.player_sprite[0].change_x = PLAYER_MOVEMENT_SPEED
         self.player_list.append(self.player_sprite[0])
 
         #Initialize the last position:
         self.last_position          = self.player_sprite[0].right
-        # Create the 'physics engine' to the first sprite of the player.
+        # Create the 'physics engine'
         self.physics_engine.append(arcade.PhysicsEnginePlatformer(self.player_sprite[0],
                                                              self.wall_list,
                                                              GRAVITY))
@@ -132,13 +143,18 @@ class MyGame(arcade.View):
         # Clear the screen to the background color. It is required to be called
         # before drawing anything to the screen.
         arcade.start_render()
+        
+        
+        arcade.draw_lrwh_rectangle_textured(self.total_distance, self.total_distance, self.total_distance+SCREEN_WIDTH, self.total_distance+SCREEN_HEIGHT,
+                                            self.background)
 
-        #Create new coins and obstacles if the player walked enough distance.
+        #create new coins and obstacles
         if (self.player_sprite[0].right - self.last_position >= self.obstacle_distance):
-            self.last_position = self.player_sprite[0].right #reset last position.
+            self.last_position = self.player_sprite[0].right
             self.create_obstacles_and_coins()
 
-        #Calculate the player's speed and distance:
+
+        # Draw our score on the screen, scrolling it with the viewport:
         if (self.speed != self.player_sprite[0].change_x):
             self.distance    += self.speed*(time.time() - self.initial_time)
             self.speed        = self.player_sprite[0].change_x
@@ -147,8 +163,7 @@ class MyGame(arcade.View):
         if (delta_t*self.speed + self.distance - self.total_distance >= 100):
             self.total_distance += 100
             self.increase_score(10)
-
-        #Print the information (score, speed and distance walked) on the screen, scrolling it with the viewport:
+        #Print the information on the screen
         score_text       = "Pontuação total: {0:<5} (Parcial: {1:<5})".format(self.total_score, self.score)
         speed_text       = "Velocidade: {:<5.0f} m/s".format(self.speed)
         total_distance   = "Distância total: {:<5.0f} m".format(self.total_distance)
@@ -171,14 +186,14 @@ class MyGame(arcade.View):
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
-        if key == arcade.key.UP or key == arcade.key.W: #Jump
+        if key == arcade.key.UP or key == arcade.key.W:
             for player in self.player_sprite:
                 player.change_y  = PLAYER_JUMP_SPEED + 20*random.random() - 5
             arcade.play_sound(self.jump_sound)
-        elif key == arcade.key.F: #Set fullscreen
+        elif key == arcade.key.F:
             self.window.set_fullscreen(not self.window.fullscreen)
             self.window.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
-        elif key == arcade.key.P: # Pause game
+        elif key == arcade.key.P:
             pause_view = PauseView(self)
             self.window.show_view(pause_view)
         '''
@@ -188,7 +203,10 @@ class MyGame(arcade.View):
 
     def update(self, delta_time):
         """ Movement and game logic """
-
+        
+        arcade.draw_lrwh_rectangle_textured(self.total_distance, self.total_distance, self.total_distance+SCREEN_WIDTH, self.total_distance+SCREEN_HEIGHT,
+                                            self.background)
+        
         #Update engines:
         for engine in self.physics_engine:
             engine.update()
@@ -265,7 +283,7 @@ class MyGame(arcade.View):
             if (len(self.wall_list) >= 6):
                 self.wall_list.pop(0)
             self.wall_list.append(wall_bottom)
-        for i in range(random.randint(1, 10)):
+        for i in range(random.randint(0, 10)):
             coin = arcade.Sprite("images/items/coinGold.png", COIN_SCALING)
             coin.position = (self.player_list[0].center_x + SCREEN_WIDTH + random.random()*(SCREEN_WIDTH), random.random()*SCREEN_HEIGHT)
             self.coin_list.append(coin)
@@ -275,16 +293,14 @@ class MyGame(arcade.View):
     def level_up(self):
         self.level                   += 1
         initial_speed = self.player_sprite[0].change_x
-        self.obstacle_distance       *= 1.05 #Increase the distance between obstacles.
-
-        #Create the next clone:
+        self.obstacle_distance       *= 1.05
         self.player_sprite.append(arcade.Sprite("images/player_1/slimeBlue.png", CHARACTER_SCALING))
         self.player_sprite[-1].center_x = self.player_sprite[0].right + (30*random.random() -2.5)
         self.player_sprite[-1].last_x = self.player_sprite[-1].center_x
         self.player_sprite[-1].center_y = self.player_sprite[0].bottom + (5*random.random() -2.5)
         self.player_list.append(self.player_sprite[-1])
         for player in self.player_sprite:
-            player.change_x  = initial_speed*1.1 #Increase speed.
+            player.change_x  = initial_speed*1.1
         self.physics_engine.append(arcade.PhysicsEnginePlatformer(self.player_sprite[-1],
                                                                 self.wall_list,
                                                                 GRAVITY))
@@ -332,15 +348,13 @@ class GameOver(arcade.View):
     def __init__(self, score):
         super().__init__()
         self.score = score
+
     def on_show(self):
         arcade.set_background_color(arcade.color.BLACK)
 
 
     def on_draw(self):
-        # Clear the screen to the background color. It is required to be called
-        # before drawing anything to the screen.
         arcade.start_render()
-
         self.window.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
         arcade.draw_text("GAME OVER", SCREEN_WIDTH/2, SCREEN_HEIGHT/2+50,
                          arcade.color.RED, font_size=50, anchor_x="center")
